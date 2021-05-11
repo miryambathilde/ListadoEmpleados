ea5465252a0f385dfaaee5f5fe293de4edbd9dc8
miryambathilde <mbathilde@gmail.com>
2021-05-11 14:00:54 +0200

    creamos modelo de la clase empleado

---
+++ src/app/models/Empleado.ts
@@ -0,0 +1,19 @@
+//creamos el modelo de la clase empleado con los campos que tiene y definimos el tipo
+
+export class Empleado {
+  numeroEmpleado: number;
+  nombre: string;
+  apellidos: string;
+  sexo: string;
+  salario: number;
+
+  //para iniciar esta clase tenemos que crear el constructor y le pasamos por parametros las varibles de las propiedades definidas:
+  constructor(numeroEmpleado: number, nombre: string, apellidos: string, sexo: string, salario: number) {
+    this.numeroEmpleado = numeroEmpleado;
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.sexo = sexo;
+    this.salario = salario;
+  }
+
+}
